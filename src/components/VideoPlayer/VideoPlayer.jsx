289ba7df302d843cb30c62afb1030b77719b275b
Eo Soo Kim <eosookim@gmail.com>
2024-05-28 16:41:06 -0400

    add background fill and styling

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,13 +1,19 @@
-import './VideoPlayer.scss'
+import "./VideoPlayer.scss";
 
-function VideoPlayer(){
-    return (
-        <>
-            <video className="player" controls>
-                <source src="https://unit-3-project-api-0a5620414506.herokuapp.com/stream"></source>
-            </video>
-        </>
-    )
+function VideoPlayer() {
+  return (
+    <>
+      <div className="player">
+        <video
+          className="player__video"
+          poster="https://unit-3-project-api-0a5620414506.herokuapp.com/images/image0.jpg"
+          controls
+        >
+          <source src=""></source>
+        </video>
+      </div>
+    </>
+  );
 }
 
 export default VideoPlayer;
