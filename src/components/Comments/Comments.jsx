168605cf3b3e45ae222f665fc83f231d95e6df5c
Eo Soo Kim <eosookim@gmail.com>
2024-05-29 11:52:05 -0400

    create component for a single comment

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,9 +1,23 @@
 import './Comments.scss'
+import SingleComment from './SingleComment/SingleComments';
 
 function Comments({commentsList}){
+    
     return(
-        <>
-        </>
+        <div className="comments">
+            <div className="comments__number"></div>
+            <form className="comments__form">
+                <img></img>
+                <label for="comment">join the conversation</label>
+                <textarea name="comment"></textarea>
+                <button type="submit">comment</button>
+            </form>
+            {commentsList.map((comment) => {
+                return (
+                    <SingleComment name={comment.name} comment={comment.comment} time={comment.timestamp}/>
+                )
+            })}
+        </div>
     )
 }
 
---
+++ src/components/Comments/SingleComment/SingleComments.jsx
@@ -0,0 +1,12 @@
+import './SingleComment.scss'
+
+function SingleComment({name, comment, time}){
+    console.log(name);
+    console.log(comment);
+    return (
+        <>
+        </>
+    )
+}
+
+export default SingleComment;
