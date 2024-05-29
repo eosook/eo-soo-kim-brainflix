c265df5a7ae9354b955c445ced6336bde2cd786a
Eo Soo Kim <eosookim@gmail.com>
2024-05-29 14:43:31 -0400

    add styling for SingleComments component

--- src/components/Comments/SingleComment/SingleComments.jsx
+++ src/components/Comments/SingleComment/SingleComments.jsx
@@ -3,9 +3,18 @@ import './SingleComment.scss'
 function SingleComment({name, comment, time}){
     console.log(name);
     console.log(comment);
+    let date = new Date(time);
     return (
-        <>
-        </>
+        <div className="comment">
+            <div className="comment__avatar"></div>
+            <div className="comment__info">
+                <div className="comment__divider">
+                    <div className="comment__name">{name}</div>
+                    <div className="comment__date">{date.toLocaleDateString('en-US')}</div>
+                </div>
+                <div className="comment__comment">{comment}</div>
+            </div>
+        </div>
     )
 }
 
