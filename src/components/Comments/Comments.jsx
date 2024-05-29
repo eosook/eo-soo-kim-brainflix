80d9f95476d751b7bff3871678a28f0daa1a9d6c
Eo Soo Kim <eosookim@gmail.com>
2024-05-29 14:56:01 -0400

    format with prettier

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,28 +1,39 @@
-import './Comments.scss'
-import SingleComment from './SingleComment/SingleComments';
+import "./Comments.scss";
+import SingleComment from "./SingleComment/SingleComments";
 
-function Comments({commentsList}){
-    
-    return(
-        <div className="comments">
-            <div className="comments__number">{commentsList.length} comments</div>
-            <form className="comments-form">
-                <div className="comments-form__image" title="avatar"></div>
-                <div className="comments__divider">
-                    <label className="comments-form__label" for="comment">join the conversation</label>
-                    <div className="comments__input">
-                        <textarea className="comments-form__textarea" name="comment" placeholder="Add a new comment"></textarea>
-                        <button className="comments-form__button" type="submit">comment</button>
-                    </div>
-                </div>
-            </form>
-            {commentsList.map((comment) => {
-                return (
-                    <SingleComment name={comment.name} comment={comment.comment} time={comment.timestamp}/>
-                )
-            })}
+function Comments({ commentsList }) {
+  return (
+    <div className="comments">
+      <div className="comments__number">{commentsList.length} comments</div>
+      <form className="comments-form">
+        <div className="comments-form__image" title="avatar"></div>
+        <div className="comments__divider">
+          <label className="comments-form__label" for="comment">
+            join the conversation
+          </label>
+          <div className="comments__input">
+            <textarea
+              className="comments-form__textarea"
+              name="comment"
+              placeholder="Add a new comment"
+            ></textarea>
+            <button className="comments-form__button" type="submit">
+              comment
+            </button>
+          </div>
         </div>
-    )
+      </form>
+      {commentsList.map((comment) => {
+        return (
+          <SingleComment
+            name={comment.name}
+            comment={comment.comment}
+            time={comment.timestamp}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default Comments;
--- src/components/Comments/SingleComment/SingleComments.jsx
+++ src/components/Comments/SingleComment/SingleComments.jsx
@@ -1,21 +1,23 @@
-import './SingleComment.scss'
+import "./SingleComment.scss";
 
-function SingleComment({name, comment, time}){
-    console.log(name);
-    console.log(comment);
-    let date = new Date(time);
-    return (
-        <div className="comment">
-            <div className="comment__avatar"></div>
-            <div className="comment__info">
-                <div className="comment__divider">
-                    <div className="comment__name">{name}</div>
-                    <div className="comment__date">{date.toLocaleDateString('en-US')}</div>
-                </div>
-                <div className="comment__comment">{comment}</div>
-            </div>
+function SingleComment({ name, comment, time }) {
+  console.log(name);
+  console.log(comment);
+  let date = new Date(time);
+  return (
+    <div className="comment">
+      <div className="comment__avatar"></div>
+      <div className="comment__info">
+        <div className="comment__divider">
+          <div className="comment__name">{name}</div>
+          <div className="comment__date">
+            {date.toLocaleDateString("en-US")}
+          </div>
         </div>
-    )
+        <div className="comment__comment">{comment}</div>
+      </div>
+    </div>
+  );
 }
 
 export default SingleComment;
