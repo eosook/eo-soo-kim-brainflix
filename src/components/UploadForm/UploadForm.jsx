89202085f39dfa777f20a564fc72b688bfd68a2b
Eo Soo Kim <eosookim@gmail.com>
2024-06-03 17:21:26 -0400

    responsive design for tablet screens

--- src/components/UploadForm/UploadForm.jsx
+++ src/components/UploadForm/UploadForm.jsx
@@ -2,32 +2,42 @@ import "./UploadForm.scss";
 import uploadThumbnail from "../../assets/images/Upload-video-preview.jpg";
 
 function UploadForm() {
-    console.log(uploadThumbnail);
+  console.log(uploadThumbnail);
   return (
     <form className="upload-form">
-      <h2 className="upload-form__label">video thumbnail</h2>
-      <img className="upload-form__thumbnail" src={uploadThumbnail}></img>
-      <label className="upload-form__label" htmlFor="title">
-        title your video
-      </label>
-      <input
-        className="upload-form__title"
-        type="text"
-        name="title"
-        placeholder="Add a title to your video"
-      ></input>
-      <label className="upload-form__label" htmlFor="description">
-        add a video description
-      </label>
-      <textarea
-        className="upload-form__description"
-        name="desciption"
-        placeholder="Add a description to your video"
-      ></textarea>
-      <button className="upload-form__submit" type="submit">publish</button>
-      <a className="upload-form__cancel">
-        <h2 className="upload-form__cancel--font">cancel</h2>
-      </a>
+      <div className="upload-form__divider">
+        <div>
+          <h2 className="upload-form__label">video thumbnail</h2>
+          <img className="upload-form__thumbnail" src={uploadThumbnail}></img>
+        </div>
+        <div className="upload-form__inputs">
+          <label className="upload-form__label" htmlFor="title">
+            title your video
+          </label>
+          <input
+            className="upload-form__title"
+            type="text"
+            name="title"
+            placeholder="Add a title to your video"
+          ></input>
+          <label className="upload-form__label" htmlFor="description">
+            add a video description
+          </label>
+          <textarea
+            className="upload-form__description"
+            name="desciption"
+            placeholder="Add a description to your video"
+          ></textarea>
+        </div>
+      </div>
+      <div className="upload-form__buttons">
+        <button className="upload-form__publish" type="submit">
+          publish
+        </button>
+        <a className="upload-form__cancel">
+          <h2 className="upload-form__cancel--font">cancel</h2>
+        </a>
+      </div>
     </form>
   );
 }
