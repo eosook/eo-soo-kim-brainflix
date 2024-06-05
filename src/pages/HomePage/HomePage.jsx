dbea110ab2cf94ed0a502e40539c4be1e8e9dc7c
Eo Soo Kim <eosookim@gmail.com>
2024-06-05 14:48:47 -0400

    clicking video now navigates to proper url

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,36 +1,39 @@
 import "./HomePage.scss";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import VideoPlayer from "../../components/VideoPlayer/VideoPlayer";
 import VideoInfo from "../../components/VideoInfo/VideoInfo";
 import Comments from "../../components/Comments/Comments";
 import VideoList from "../../components/VideoList/VideoList";
 
 function HomePage() {
-    const [currentId, setCurrentId] = useState("84e96018-4022-434e-80bf-000ce4cd12b8");
-    const [videoData, setVideoData] = useState({
-      id: "",
-      title: "",
-      channel: "",
-      image: "",
-      description: "",
-      timestamp: "",
-      comments: []
-    });
-    const { videoId } = useParams();
-    const api = "d60d277a-1428-43ef-9f19-7c0c17b58240";
+  const [currentId, setCurrentId] = useState(
+    "84e96018-4022-434e-80bf-000ce4cd12b8"
+  );
+  const [videoData, setVideoData] = useState({
+    id: "",
+    title: "",
+    channel: "",
+    image: "",
+    description: "",
+    timestamp: "",
+    comments: [],
+  });
+  const { videoId } = useParams();
+  const api = "d60d277a-1428-43ef-9f19-7c0c17b58240";
+  const navigate = useNavigate();
 
   useEffect(() => {
-    const getData = async() => {
+    const getData = async () => {
       const response = await axios.get(
         `https://unit-3-project-api-0a5620414506.herokuapp.com/videos/${currentId}?api_key="${api}"`
-      )
+      );
       setVideoData(response.data);
-    }
+    };
     getData();
   }, [currentId]);
-    
+
   useEffect(() => {
     const getData = async () => {
       const response = await axios.get(
@@ -41,8 +44,9 @@ function HomePage() {
     getData();
   }, [videoId]);
 
-  function changeVideo(videoId){
-    setCurrentId(videoId)
+  function changeVideo(videoId) {
+    setCurrentId(videoId);
+    navigate(`/video/${videoId}`);
   }
 
   return (
@@ -54,7 +58,7 @@ function HomePage() {
           <Comments commentsList={videoData.comments} />
         </div>
         <div className="main__divider">
-          <VideoList currentId={currentId} changeVideo={changeVideo}/>
+          <VideoList currentId={currentId} changeVideo={changeVideo} />
         </div>
       </div>
     </>
