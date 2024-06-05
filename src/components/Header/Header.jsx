89d4d47ce04ab7fdabed1a99941fba6f39b2bdb3
Eo Soo Kim <eosookim@gmail.com>
2024-06-05 17:14:42 -0400

    change onclick function to Link to

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import logo from "../../assets/images/logo/BrainFlix-logo.svg";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Header.scss";
 
 function Header() {
@@ -7,16 +8,25 @@ function Header() {
 
   return (
     <header className="header">
-      <img src={logo} alt="BrainFlix Logo" className="header__logo" onClick={()=> {navigate('/')}}></img>
+      <img
+        src={logo}
+        alt="BrainFlix Logo"
+        className="header__logo"
+        onClick={() => {
+          navigate("/");
+        }}
+      ></img>
       <form className="form">
         <input
           type="text"
           className="form__search"
           placeholder="Search"
         ></input>
-        <button type="submit" className="form__submit" onClick={()=> {navigate('/upload')}}>
-          upload
-        </button>
+        <Link to="/upload">
+          <button type="submit" className="form__submit">
+            upload
+          </button>
+        </Link>
         <div className="form__avatar"></div>
       </form>
     </header>
