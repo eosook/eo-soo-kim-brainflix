60f0829fb8c32039bd1c8f3c2dc0384b9d517032
Eo Soo Kim <eosookim@gmail.com>
2024-06-14 17:12:50 -0400

    form in uploadform now posts to api

--- src/components/UploadForm/UploadForm.jsx
+++ src/components/UploadForm/UploadForm.jsx
@@ -1,20 +1,41 @@
 import "./UploadForm.scss";
 import { useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 function UploadForm() {
   const navigate = useNavigate();
   const uploadThumbnail = "http://localhost:5050/images/Upload-video-preview.jpg";
+  const uploadUrl = "http://localhost:5050/upload";
 
   function navHome() {
     navigate("/");
   }
+
+  function handleSubmit(event){
+    event.preventDefault();
+    const newDate = Date.now();
+    const newVideo = {
+      id: newDate.toString(),
+      image : event.target.thumbnail.src,
+      title: event.target.title.value,
+      channel: "Eo Soo Kim",
+      description: event.target.description.value,
+      views: "0",
+      likes: "0",
+      duration: "12:20",
+      timestamp: newDate,
+      comments: []
+    }
+    axios.post(uploadUrl, newVideo);
+    navigate("/");
+  }
   return (
-    <form className="upload-form">
+    <form className="upload-form" onSubmit={handleSubmit}>
       <h1 className="upload-form__header">Upload Video</h1>
       <div className="upload-form__divider">
         <div>
           <h2 className="upload-form__label">video thumbnail</h2>
-          <img className="upload-form__thumbnail" src={uploadThumbnail}></img>
+          <img className="upload-form__thumbnail" src={uploadThumbnail} name="thumbnail" value="image"></img>
         </div>
         <div className="upload-form__inputs">
           <label className="upload-form__label" htmlFor="title">
@@ -31,7 +52,7 @@ function UploadForm() {
           </label>
           <textarea
             className="upload-form__description"
-            name="desciption"
+            name="description"
             placeholder="Add a description to your video"
           ></textarea>
         </div>
@@ -40,7 +61,6 @@ function UploadForm() {
         <button
           className="upload-form__publish"
           type="submit"
-          onClick={navHome}
         >
           publish
         </button>
